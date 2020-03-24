ea456a98ef5dd08eea880f18505fe0d4aaf8be32
Hugo Lesta <hlesta@edrans.com>
2020-03-24 20:30:45 -0300

    Deleted empty lines

--- exportAwsCredentials.js
+++ exportAwsCredentials.js
@@ -3,9 +3,7 @@ const ini = require("ini");
 const homedir = require('os').homedir();
 const credentialsFilePath = `${homedir}/.aws/credentials`;
 const awsProfile = process.argv[2] || "edrans";
-
 const credentialsIni = ini.parse(fs.readFileSync(credentialsFilePath, "utf-8"));
-
 
 console.log(
   `export AWS_ACCESS_KEY_ID=${credentialsIni[awsProfile].aws_access_key_id}`,
