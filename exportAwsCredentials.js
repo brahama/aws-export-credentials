efde3406affc2df2449d1ec7494f64d524e6d960
Hugo Lesta <hlesta@edrans.com>
2020-03-24 20:29:33 -0300

    Added homedir constant and new console.log format

--- exportAwsCredentials.js
+++ exportAwsCredentials.js
@@ -1,20 +1,14 @@
 const fs = require("fs");
 const ini = require("ini");
-const credentialsFilePath = "/[YOUR HOME HERE]/.aws/credentials";
+const homedir = require('os').homedir();
+const credentialsFilePath = `${homedir}/.aws/credentials`;
 const awsProfile = process.argv[2] || "edrans";
 
-console.log(awsProfile);
 const credentialsIni = ini.parse(fs.readFileSync(credentialsFilePath, "utf-8"));
 
+
 console.log(
-  "export AWS_ACCESS_KEY_ID=%s",
-  credentialsIni[awsProfile].aws_access_key_id
-);
-console.log(
-  "export AWS_SECRET_ACCESS_KEY=%s",
-  credentialsIni[awsProfile].aws_secret_access_key
-);
-console.log(
-  'export AWS_SESSION_TOKEN="%s"',
-  credentialsIni[awsProfile].aws_session_token
+  `export AWS_ACCESS_KEY_ID=${credentialsIni[awsProfile].aws_access_key_id}`,
+  `AWS_SECRET_ACCESS_KEY=${credentialsIni[awsProfile].aws_secret_access_key}`,
+  credentialsIni[awsProfile].aws_session_token ? `AWS_SESSION_TOKEN=${credentialsIni[awsProfile].aws_session_token}`:''
 );
