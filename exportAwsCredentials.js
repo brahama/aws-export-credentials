239515a0bd5de3b51028ad4060abaa882f1faba0
Hugo Lesta <hlesta@edrans.com>
2020-03-24 21:18:42 -0300

    Added profile in pare line

--- exportAwsCredentials.js
+++ exportAwsCredentials.js
@@ -3,10 +3,10 @@ const ini = require("ini");
 const homedir = require('os').homedir();
 const credentialsFilePath = `${homedir}/.aws/credentials`;
 const awsProfile = process.argv[2] || "edrans";
-const credentialsIni = ini.parse(fs.readFileSync(credentialsFilePath, "utf-8"));
+const credentialsIni = ini.parse(fs.readFileSync(credentialsFilePath, "utf-8"))[awsProfile];
 
 console.log(
-  `export AWS_ACCESS_KEY_ID=${credentialsIni[awsProfile].aws_access_key_id}`,
-  `AWS_SECRET_ACCESS_KEY=${credentialsIni[awsProfile].aws_secret_access_key}`,
-  credentialsIni[awsProfile].aws_session_token ? `AWS_SESSION_TOKEN=${credentialsIni[awsProfile].aws_session_token}`:''
+  `export AWS_ACCESS_KEY_ID=${credentialsIni.aws_access_key_id}`,
+  `AWS_SECRET_ACCESS_KEY=${credentialsIni.aws_secret_access_key}`,
+  credentialsIni.aws_session_token ? `AWS_SESSION_TOKEN=${credentialsIni.aws_session_token}`:''
 );
