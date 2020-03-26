ace42e8f29e93eb574b385f94ec6e1f97e14141a
Hugo Lesta <hlesta@edrans.com>
2020-03-26 09:18:23 -0300

    Added doble quote at the beggining and the end of the variable

--- exportAwsCredentials.js
+++ exportAwsCredentials.js
@@ -6,7 +6,7 @@ const awsProfile = process.argv[2] || "edrans";
 const credentialsIni = ini.parse(fs.readFileSync(credentialsFilePath, "utf-8"))[awsProfile];
 
 console.log(
-  `export AWS_ACCESS_KEY_ID=${credentialsIni.aws_access_key_id}`,
-  `AWS_SECRET_ACCESS_KEY=${credentialsIni.aws_secret_access_key}`,
-  credentialsIni.aws_session_token ? `AWS_SESSION_TOKEN=${credentialsIni.aws_session_token}`:''
+  `export AWS_ACCESS_KEY_ID="${credentialsIni.aws_access_key_id}"`,
+  `AWS_SECRET_ACCESS_KEY="${credentialsIni.aws_secret_access_key}"`,
+  credentialsIni.aws_session_token ? `AWS_SESSION_TOKEN="${credentialsIni.aws_session_token}"`:''
 );
